c739f85cc81916aa4beec31fc81906d10f262e01
tidalboot <tidalboot@googlemail.com>
2015-03-26 23:42:08 +0000

    Finished off the additionGameSceneView

--- CountingDaisy/CountingDaisy/AdditionGameViewController.swift
+++ CountingDaisy/CountingDaisy/AdditionGameViewController.swift
@@ -13,36 +13,63 @@ class AdditionGameViewController: UIViewController {
     let randomNumberCalculator = RandomNumberCalculator()
     let additionGameHandler = AdditionGameHandler()
     
+    var augend: Int = 0
+    var addend: Int = 0
+    var summation: Int = 0
+    var score: Int = 0
+    
     @IBOutlet var randomNumberLabel: UILabel!
     @IBOutlet var augendLabel: UILabel!
     @IBOutlet var addendLAbel: UILabel!
     @IBOutlet var summationLabel: UILabel!
+    @IBOutlet var scoreLabel: UILabel!
+    @IBOutlet var correctLabel: UILabel!
+    @IBOutlet var incorrectLabel: UILabel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        nextSetOfNumbers()
+        incorrectLabel.hidden = true
+        correctLabel.hidden = true
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    
-    @IBAction func generateRandomNumber(sender: AnyObject) {
-        
+    func nextSetOfNumbers () {
         var arrayOfRandomNumbers = randomNumberCalculator.generateRandomNumbers(2, minumumValue: 1, maximumValue: 50)
         
-        var augend = arrayOfRandomNumbers[0] as! Int
-        var addend = arrayOfRandomNumbers[1] as! Int
-        
+        augend = arrayOfRandomNumbers[0] as! Int
+        addend = arrayOfRandomNumbers[1] as! Int
+        summation = additionGameHandler.generateResult(augend, addend: addend)
         augendLabel.text = "\(augend)"
         addendLAbel.text = "\(addend)"
-        summationLabel.text = "\(additionGameHandler.generateResult(augend, addend: addend))"
-        
+        summationLabel.text = "\(summation)"
+    }
+    
+    @IBAction func clickedYes(sender: AnyObject) {
+        if summation == (augend + addend) {
+            score++
+            scoreLabel.text = "\(score)"
+            incorrectLabel.hidden = true
+            correctLabel.hidden = false
+        }
+        else {
+            incorrectLabel.hidden = false
+            correctLabel.hidden = true
+        }
+        nextSetOfNumbers()
     }
 
+    @IBAction func clickedNo(sender: AnyObject) {
+        if summation != (augend + addend) {
+            score++
+            scoreLabel.text = "\(score)"
+            incorrectLabel.hidden = true
+            correctLabel.hidden = false
+        }
+        else {
+            incorrectLabel.hidden = false
+            correctLabel.hidden = true
+        }
+        nextSetOfNumbers()
+    }
 }
