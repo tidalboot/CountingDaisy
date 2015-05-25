035479306cc2b10ee785ba4d2577dc48e1fb5142
tidalboot <tidalboot@googlemail.com>
2015-05-25 23:10:28 +0100

    Huge UI and design overhaul, new design implemented

--- Kazu/Kazu/MainMenuViewController.swift
+++ Kazu/Kazu/MainMenuViewController.swift
@@ -2,14 +2,22 @@
 import UIKit
 
 class MainMenuViewController: UIViewController {
-
+        
+    @IBOutlet var additionImage: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let loadGameScene = UITapGestureRecognizer(target: self, action: "userTappedScreen")
+        additionImage.addGestureRecognizer(loadGameScene)
     }
-
+    
+    func userTappedScreen () {
+        self.performSegueWithIdentifier("gameScene", sender: self)
+    }
+    
     override func preferredStatusBarStyle() -> UIStatusBarStyle {
         return UIStatusBarStyle.LightContent
     }
-
 }
 
